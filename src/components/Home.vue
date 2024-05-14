fe965464f2b3108ea3f24dd669341bd166066d04
EverthMartinez3002 <00156021@uca.edu.sv>
2024-05-13 19:45:29 -0600

    configuracion inicial

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+
+  <div class="card">
+    <p>      
+      <v-btn color="#000000">Este sera el home</v-btn>
+    </p>
+  </div>
+
+</template>
---
+++ src/routes/router.js
@@ -0,0 +1,20 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+import HelloWorld from '../components/HelloWorld.vue'
+import Home from "../components/Home.vue";
+
+const routes = [
+    {
+        path: '/', component: Home,
+    },
+    {
+        path: '/hello', component: HelloWorld
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router;
