0650f16e4dbe5a33a704f7c5a729ffcf1dfa9dcf
EverthMartinez3002 <00156021@uca.edu.sv>
2024-05-16 18:29:16 -0600

    Feat: Vista Home Avanzada

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,58 @@
 <template>
+<div class="home-container">
 
-  <div class="card">
-    <p>      
-      <v-btn color="#000000">Este sera el home</v-btn>
-    </p>
+  <div class="w-100 d-flex justify-center flex-column" style="align-items: center;">
+  <img src="/src/assets/img/logo_HLVS.png" alt="logo_HLVS" class="img-logo">
+
+  <h2 class="mt-5">H L V S</h2>
+
+  <h3 class="mt-5">Bienvenido/a al sistema de entrada de la residencial HLVS</h3>
+
+  <v-btn class="button-google" style="background-color: #F1F5F9; width: 300px;">
+    <img src="/src/assets/img/google-icon.svg" alt="Google Icon" class="google-icon"/>
+    Continua con Google</v-btn>
   </div>
 
+  <div class="d-flex justify-end w-100">
+  <img src="/src/assets/img/casa_alargada.png" alt="casa_logo" class="w-100">
+  </div>
+
+</div>
+
+
 </template>
+
+<style scoped>
+
+.home-container{
+  display: flex;
+}
+
+.img-logo {
+  width: 150px;
+  height: 150px;
+}
+
+.button-google{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: var(--Semi-rounded, 12px);
+background: #F1F5F9;
+box-shadow: 8px 8px 16px 0px rgba(201, 217, 232, 0.70), -8px -8px 16px 0px #FFF;
+padding: 14px 32px 14px 16px;
+margin-top: 2em;
+}
+
+.google-icon {
+  width: 20px;
+  height: 20px;
+  margin-right: 20px;
+}
+
+@media (min-width: 768px) {
+  .div {
+    width: 50%;
+  }
+}
+</style>
