acc39701475660fa20c2e1d1a2731a550823fc1f
Everth_Martinez <jemartinez@innovacion.gob.sv>
2024-05-30 10:21:37 -0600

    Ajustes tabla historial

--- src/components/historytable.vue
+++ src/components/historytable.vue
@@ -1,7 +1,19 @@
 <template>
 <v-data-table :headers="headers" :items="entries" item-key="id" items-per-page="10" class="mt-5"
- style="background-color: #EDF1F4; font-family: 'Josefin Sans', sans-serif;"
+style="background-color: #EDF1F4; font-family: 'Josefin Sans', sans-serif;"
 >
+<template v-slot:header.nombre="{ column }">
+ <span class="title">{{ column.title }}</span> 
+</template>
+<template v-slot:header.fecha="{ column }">
+ <span class="title">{{ column.title }}</span> 
+</template>
+<template v-slot:header.acceso="{ column }">
+ <span class="title">{{ column.title }}</span> 
+</template>
+<template v-slot:header.hora="{ column }">
+ <span class="title">{{ column.title }}</span> 
+</template>
 </v-data-table>
 </template>
   
@@ -26,5 +38,13 @@
   </script>
   
   <style scoped>
+  .title{
+    font-size: 24px;
+  }
+  @media (max-width: 768px) {
+  .title{
+    font-size: 14px;
+  }
+}
   </style>
   
