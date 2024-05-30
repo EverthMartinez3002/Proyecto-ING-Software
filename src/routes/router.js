d95153cba348710318adaf1c28b67efa453ee38b
EverthMartinez3002 <00156021@uca.edu.sv>
2024-05-29 19:54:27 -0600

    view family-management avance

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,7 @@ import Home from "../views/Home.vue";
 import QrGenerator from "../views/QrGenerator.vue";
 import History from "../views/History.vue";
 import RequestFamily from "../views/requestFamily.vue";
+import FamilyManagement from "../views/family-management.vue";
 
 const routes = [
     {
@@ -17,6 +18,9 @@ const routes = [
     },
     {
         path: '/family-request', component: RequestFamily
+    },
+    {
+        path: '/family-management', component: FamilyManagement
     }
 ];
 
---
+++ src/views/family-management.vue
@@ -0,0 +1,136 @@
+<template>
+<Navbar />
+
+<div class="d-flex justify-center">
+<h3 class="josefin-sans" style="color: #000; font-size: 36px; margin-top: 1em;">Gestión de residentes de tu casa</h3>
+</div>
+
+<div class="d-flex justify-center form-container">
+    <div v-for="(resident, index) in residents" :key="index" class="resident-form">
+      <div class="field-container">
+    <h3 class="josefin-sans" style="color: #000;">Email*</h3>
+      <v-text-field
+        v-model="resident.email"
+        hide-details="auto"
+        label="Email*"
+        class="email-field"
+        style="width: 300px;"
+      ></v-text-field>
+      </div>
+      <div class="field-container">
+        <h3 class="josefin-sans" style="color: #000;">DUI</h3>
+      <v-text-field
+        v-model="resident.dui"
+        hide-details="auto" 
+        label="DUI"
+        class="dui-field"
+      ></v-text-field>
+      </div>
+      <v-btn icon @click="addResident" class="add-btn" :disabled="residents.length >= 3">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-btn icon @click="removeResident(index)" class="remove-btn" :disabled="residents.length <= 1">
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+    </div>
+  </div>
+
+  <div class="d-flex justify-center">
+  <v-btn class="josefin-sans btn-actualizar" style="margin-top: 4em; margin-bottom: 1em;" v-if="!isQRActive">
+    <span style="text-transform: none; font-size: 18px;" class="jostfin-sans">Actualizar</span>
+    </v-btn>
+</div> 
+
+</template>
+
+<script>
+import Navbar from '../components/navbar.vue';
+export default {
+components: {
+  Navbar,
+},
+data() {
+    return {
+      residents: [
+        { email: '', dui: '' },
+        { email: '', dui: '' }
+      ]
+    };
+  }
+,
+methods: {
+    addResident() {
+        if (this.residents.length < 3) {
+            this.residents.push({ email: '', dui: '' });
+        }    
+    },
+    removeResident(index) {
+        if (this.residents.length > 1) {
+            this.residents.splice(index, 1);
+        }
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+.field-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-container{
+    flex-direction: column;
+    width: 50%;
+    margin: auto;
+    margin-top: 3em;
+}
+
+.resident-form {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 1em;
+}
+
+.email-field{
+   width: 300px;
+   border-radius: 8px;
+   background-color: white;
+   border: 1px solid #12453B;
+}
+
+.dui-field{
+    width: 209px;
+    border-radius: 8px;
+    background-color: white;
+    border: 1px solid #12453B;
+}
+
+.add-btn,
+.remove-btn {
+  min-width: 36px;
+  min-height: 36px;
+  background-color: #12453B;
+  color: #EDF1F4;
+}
+
+.update-btn {
+  width: 150px;
+  height: 45px;
+  margin-top: 1em;
+  background-color: #12453B;
+  color: #EDF1F4;
+  border-radius: 10px;
+}
+
+.btn-actualizar{
+  color: #FFF;
+  width: 169px;
+  height: 48px;
+  background: #12453B;
+  border-radius: 20px;
+}
+</style>
