d8ebaae69c28d60e28259cb52a0ac090b25eefe6
EverthMartinez3002 <00156021@uca.edu.sv>
2024-11-22 22:44:39 -0600

    Proyectos fusionados

--- frontend/src/plugins/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VUE_APP_API_URL || 'http://3.140.250.202:8080',
- });
-
- export default axiosInstance;
